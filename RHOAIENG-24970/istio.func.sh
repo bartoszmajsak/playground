cf2341acbf908e78468ce43678224047bc2515cf
Bartosz Majsak <bartosz.majsak@gmail.com>
2025-05-29 22:18:04 +0200

    fix: ensures new authz provider is not duplicated on subsequent patches

--- RHOAIENG-24970/istio.func.sh
+++ RHOAIENG-24970/istio.func.sh
@@ -5,27 +5,29 @@ configure_external_authz_provider() {
   local ns="istio-system"
   local provider_yaml="manifests/provider.config.yaml"
 
-  local mesh_yaml=$(kubectl -n "$ns" get configmap "$cm_name" -o json | jq -r '.data.mesh // ""')
-  [[ -z "$mesh_yaml" ]] && mesh_yaml="{}"
+  local mesh_yaml_str
+  mesh_yaml_str=$(kubectl -n "$ns" get configmap "$cm_name" -o json | jq -r '.data.mesh // "{}"')
 
-  local provider_name=$(yq '.name' "$provider_yaml")
-  if echo "$mesh_yaml" | yq '.extensionProviders[].name' 2>/dev/null | grep -qx "$provider_name"; then
-    echo "extensionProvider '$provider_name' already present in ConfigMap $cm_name in namespace $ns. Skipping."
-    return 0
-  fi
+  local new_provider_json
+  new_provider_json=$(yq -o=json '.' "$provider_yaml" | jq '.extensionProviders[0]')
+
+  local provider_name
+  provider_name=$(echo "$new_provider_json" | jq -r '.name' | xargs)
+
+  local filtered_providers_json
+  filtered_providers_json=$(echo "$mesh_yaml_str" | yq -o=json '.extensionProviders // []' | jq "[.[] | select(.name != \"$provider_name\")]")
+
+  local updated_providers_json
+  updated_providers_json=$(echo "$filtered_providers_json" | jq ". + [$new_provider_json]")
 
   local new_mesh_yaml
-  new_mesh_yaml=$(yq eval-all '
-    . as $item ireduce ({}; . *+ $item) |
-    .extensionProviders = (.extensionProviders // [] + [.[1]])
-  ' <(echo "$mesh_yaml") "$provider_yaml")
+  new_mesh_yaml=$(echo "$mesh_yaml_str" | yq ".extensionProviders = $updated_providers_json")
 
   kubectl -n "$ns" patch configmap "$cm_name" --type='json' -p="[
     {
       \"op\": \"replace\",
       \"path\": \"/data/mesh\",
-      \"value\": $(jq -Rs . <<<"$new_mesh_yaml")
+      \"value\": $(jq -Rs . <<<\"$new_mesh_yaml\")
     }
   ]"
-
 }
